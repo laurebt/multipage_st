e1ba371d84f5167442b9f8a64bfec29dec779cfd
laurebt <laurebt@users.noreply.github.com>
2022-07-19 08:11:49 -0600

    Update app.py

--- app.py
+++ app.py
@@ -1,24 +1,34 @@
+# Copyright (c) 2021 Belmont Technology Inc. All rights reserved.
+
+from math import sqrt
+import random
 import streamlit as st
 
-def hello(name="World"):
-  '''
-  Simple hello function
-  Parameters:
-  - name (str) : any string
-  Returns:
-  - a string
-  '''
-  return "Hello " + str(name) + ", from the Daisi platform"
+#######################################################
+def check_status(n):
 
-def st_ui():
-  '''Function to render the Streamlit UI'''
+	prime_flag = 0
 
-  st.markdown("# App")
-  st.sidebar.markdown("# App")
+	if(n > 1):
+		for i in range(2, int(sqrt(n)) + 1):
+			if (n % i == 0):
+				prime_flag = 1
+				break
+		if (prime_flag == 0):
+			return True
+		else:
+			return False
+	else:
+		return False
 
-  name = st.text_input('Type your name', value = "Daisi user")
-  greeting = hello(name)
-  st.header(greeting)
+#######################################################
+if __name__ == '__main__':
 
-if __name__ == "__main__":
-  st_ui()
+	st.title("Check if prime number")
+	n = st.text_input('Insert an integer', value="0")
+
+	status = check_status(int(n))
+	if status:
+		st.text(str(n) + " is a prime number")
+	else:
+		st.text(str(n) + " is NOT a prime number")
